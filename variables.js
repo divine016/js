bfe1abed8d0f3a29567fa98aceadaae8d9340675
divine016 <koutidivine@gmail.com>
2023-08-20 22:51:10 +0100

    today did variables, datatypes type convertions

---
+++ variables.js
@@ -0,0 +1,63 @@
+// //var let const
+// var food = 'beans';
+// var food = 'rice';
+// //food = 'pink';
+// //console.log(food)
+// let greet = 'say hello';
+// greet = 'say hi instead';
+// console.log(greet) 
+
+// const fruits = 'orange';
+// fruit = 'white';
+// console.log(fruit);
+// let hello = 'say hello';
+// if (true){
+//  let hello = 'say hi hello instead'
+//  console.log(hello);
+// }
+// console.log(hello); 
+
+// hoisting
+// console.log(hello);
+// let hello = 'say hi hello instead';
+
+
+// let song = `hello hii howa re you`
+// console.log(typeof song)
+// //numbers 
+// let number = 5657545409605n
+// let number1 = 24
+// console.log(number == number1);
+
+// let symbol = symbol(`hello hii howa re you`);
+// let symbol1 = symbol(`hello hii howa re you`);
+// console.log(symbol == symbol1)
+
+// String(), Number(), Boolean();
+// let food = 'rice';
+// let food2 = Number(food);
+// console.log(typeof(food));
+// let number1 = '24';
+// let number2 = Number(number1);
+// // typeof food
+
+// let nullToNr = null;
+// console.log(typeof(nullToNr));
+// nullToNr = Number(nullToNr);
+// console.log("null", nullToNr, typeof nullToNr); //null 0 number
+// let booleanToNr = false;
+// booleanToNr = Number(nullToNr);
+// console.log("null", booleanToNr, typeof booleanToNr); //null 0 number
+
+// let strToNr = "";
+// strToNr = Number(strToNr);
+// console.log("empty string", strToNr, typeof strToNr); //empty string 0 number
+
+let name = "Divine";
+console.log(`Hey ${name} how are you`); 
+console.log("my mums name is \"eveline\"");
+console.log('my mums name is "eveline"');
+
+//name age whereyourfrom
+//hey my name is yourname my age is yourage i am from whereYour from
+
